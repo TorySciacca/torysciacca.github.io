61244b460d99aef1294a46cf5eda1b0bfe796843
TorySciacca <57762089+TorySciacca@users.noreply.github.com>
2023-05-19 23:48:40 +1000

    almost finished implementation

--- flask/app.py
+++ flask/app.py
@@ -1,13 +1,24 @@
 import sqlite3
 import bcrypt
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, g, jsonify, request
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 # Database initialization
-conn = sqlite3.connect('backend.db')
-cursor = conn.cursor()
+DATABASE = 'backend.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 # Home page
 @app.route('/')
@@ -18,36 +29,58 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username_email = request.form.get('username_email')
+        password = request.form.get('password')
 
-        cursor.execute("SELECT password FROM users WHERE username = ?", (username,))
-        result = cursor.fetchone()
+        if username_email and password:
+            conn = sqlite3.connect('backend.db')
+            cursor = conn.cursor()
 
-        if result:
-            stored_password = result[0]
-            if bcrypt.checkpw(password.encode('utf-8'), stored_password.encode('utf-8')):
-                session['username'] = username
-                return redirect(url_for('dashboard'))
-        
-        error = 'Invalid username or password'
-        return render_template('login.html', error=error)
+            # Check if the input matches either the username or email in the database
+            cursor.execute("SELECT username, password FROM users WHERE username = ? OR email = ?", (username_email, username_email))
+            result = cursor.fetchone()
+
+            if result:
+                stored_username, stored_password = result
+
+                if bcrypt.checkpw(password.encode('utf-8'), stored_password):
+                    session['username'] = stored_username
+                    conn.close()
+                    return redirect(url_for('dashboard'))
+                else:
+                    error = 'Invalid password'
+            else:
+                error = 'User does not exist'
+
+            conn.close()
+            return render_template('login.html', error=error, username_email=username_email)
 
     return render_template('login.html')
-
 # Registration page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    db = get_db()
+    cursor = db.cursor()
     if request.method == 'POST':
+        name = request.form['Name']
         username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
 
+        # Check if username or email already exists in the database
+        cursor.execute("SELECT username, email FROM users WHERE username = ? OR email = ?", (username, email))
+        existing_user = cursor.fetchone()
+        if existing_user:
+            error = 'Username or email already exists. Please choose a different username or email.'
+            return render_template('register.html', error=error)
+
+        # If username and email do not exist, proceed with registration
         salt = bcrypt.gensalt()
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
-        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed_password))
-        conn.commit()
-
+        cursor.execute("INSERT INTO users (name, username, email, password) VALUES (?, ?, ?, ?)",
+                       (name, username, email, hashed_password))
+        db.commit()
         return redirect(url_for('login'))
 
     return render_template('register.html')
@@ -56,15 +89,39 @@ def register():
 @app.route('/dashboard')
 def dashboard():
     if 'username' in session:
-        return render_template('dashboard.html', username=session['username'])
+        username = session['username']
+        conn = sqlite3.connect('backend.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT name, username FROM users WHERE username = ?", (username,))
+        result = cursor.fetchone()
+        if result:
+            name, username = result
+            conn.close()
+            return render_template('dashboard.html', name=name, username=username)
 
     return redirect(url_for('login'))
+
+@app.route('/organisation')
+def organisation():
+    return render_template('organisation.html')
 
 # Logout
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
+
+@app.route('/validate_ucn', methods=['POST'])
+def validate_ucn():
+    db = get_db()
+    cursor = db.cursor()
+    data = request.get_json()
+    ucn = data.get('UCN')
+
+    cursor.execute("SELECT username FROM users WHERE username = ?", (ucn,))
+    result = cursor.fetchone()
+
+    return jsonify({'valid': result is not None})
 
 # Run the application
 if __name__ == '__main__':
--- flask/static/scripts/script.js
+++ flask/static/scripts/script.js
@@ -1,3 +1,4 @@
+
 window.onload = function() {
     showSection('login'); // Shows the login by default when the page loads
   };
@@ -27,7 +28,7 @@ function showSection(sectionId) {
       alert("Invalid login details. Please try again."); // Display an error message
     }
   }
-
+/*
   function showValidation(event) {
     event.preventDefault()
     
@@ -50,7 +51,7 @@ function showSection(sectionId) {
         validationText.style.backgroundColor = "#e61e2a";
     }
   }
-
+*/
   function refreshPage() {
     location.reload();
   }
@@ -63,3 +64,51 @@ function showSection(sectionId) {
      console.log('download Graduate Certificate')
     }
   }
+
+  // log out function
+
+  function logout() {
+    fetch('/logout', { method: 'GET' })
+        .then(response => {
+            if (response.ok) {
+                window.location.href = '/login';  // Redirect to login page after successful logout
+            } else {
+                // Handle logout error
+                console.log('Logout failed');
+            }
+        })
+        .catch(error => {
+            // Handle logout error
+            console.log('Logout failed:', error);
+        });
+}
+
+//validates certificate
+
+function showValidation(event) {
+  event.preventDefault();
+  
+  var UCN = document.getElementById("UCN").value.trim();
+  var validationText = document.getElementById("validationText");
+
+  fetch('/validate_ucn', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ UCN: UCN })
+  })
+  .then(response => response.json())
+  .then(data => {
+      if (data.valid) {
+          validationText.textContent = "Certificate is Valid";
+          validationText.style.backgroundColor = "#5eb85c";
+      } else {
+          validationText.textContent = "Certificate is Invalid";
+          validationText.style.backgroundColor = "#e61e2a";
+      }
+  })
+  .catch(error => {
+      console.log('Error:', error);
+  });
+}
