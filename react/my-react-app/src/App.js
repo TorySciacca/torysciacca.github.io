e185d143c1fa842578cf349c11a2f4ca698e22d3
TorySciacca <57762089+TorySciacca@users.noreply.github.com>
2023-06-06 01:15:03 +1000

    egg

--- react/my-react-app/src/App.js
+++ react/my-react-app/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState } from 'react';
+
+export default function MyApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Feed Eggs</h1>
+      <div>
+      <p>egg</p>
+      <MyButton />
+      <p>egg basket</p>
+      </div>
     </div>
   );
 }
 
-export default App;
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      {count} eggs
+    </button>
+  );
+}
+
+// start with 100 eggs
+// 25 clicks = 6 eggs
+// 25 clicks = 3 eggs
+// 25 clicks = 2 eggs
+// "Dude, you ran out of eggs. Would you like to buy an 80 pack of eggs? yes/no"
+// 1 click = 40 eggs
+// 41 eggs = win
+// You're looking at aa nude egg
